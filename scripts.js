3170836066498bddba1056c739ae7e362343126b
Christina <cregelski@gmail.com>
2013-04-17 10:49:51 -0400

    Beginning Final Project
    creating a base

---
+++ scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {	
+	
+	$('#nav li').hover(
+		function () {
+			//show its submenu
+			$('ul', this).stop().slideDown(100);
+
+		}, 
+		function () {
+			//hide its submenu
+			$('ul', this).stop().slideUp(100);			
+		}
+	);
+	
+});
