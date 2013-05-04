9376f92e10e0dd83971c9cae773e4ae88201a10b
Christina <cregelski@gmail.com>
2013-05-04 19:48:31 -0400

    Adding Farm to Table pages

--- scripts.js
+++ scripts.js
@@ -14,3 +14,22 @@ $(document).ready(function () {
 	
 });
 
+
+$(document).ready(function() {
+    var ww = document.body.clientWidth;
+    if (ww < 800) {
+        $(".toggleMenu").css("display", "inline-block");
+        $(".nav li a").click(function() {
+            $(this).parent("li").toggleClass('hover');
+        });
+    } else {
+        $(".toggleMenu").css("display", "none");
+        $(".nav li").hover(function() {
+            $(this).addClass('hover');
+        }, function() {
+            $(this).removeClass('hover');
+        });
+    }
+});
+
+
